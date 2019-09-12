d02b909f0db345f2038bbb87141f21e2d7102c39
Victor Zadyabin <viktor.zadyabin@mail.ru>
2019-09-12 14:31:49 +0300

    Initial Commit

---
+++ task2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  task2
+//
+//  Created by Виктор Задябин on 12/09/2019.
+//  Copyright © 2019 Виктор Задябин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
